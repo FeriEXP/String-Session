101a7798c5ba3ebf4f0c4e96e17415783734eea3
Shin Ryujin <82243933+ferikunn@users.noreply.github.com>
2021-08-24 08:29:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,3 @@
-# Copyleft (c) 2020 Mr.Miss, All wrongs reserved.
-#
-# Redistribution and use in source with or
-# without modufication, are permitted.
-
-
-import time
-from telethon.sync import TelegramClient
-from telethon.sessions import StringSession
-from pyrogram import Client
-
-select = " "
-
-docs = """Generate your Telegram String Session
-
-P -->> Pyrogram [https://docs.pyrogram.org]
-T -->> Telethon [https://docs.telethon.dev]
-"""
-
-tutor = """
-~ go-to my.telegram.org
-~ Login using your Telegram account
-~ Click on API Development Tools
-~ Create a new application, by entering the required details
-~ Check your Telegram saved messages section to copy the STRING_SESSION
-"""
-
-template = """
-Terima Kasih Telah Menggunakan String Session @Xflicks 
-            
-<code>STRING_SESSION</code>: <code>{}</code>
-
-⚠️ <b>Please be carefull to pass this value to third parties</b>"""
-
-
 print("================= //Feri Ganteng// ================")
 print("""String Session Generator for Telethon and Pyrogram
               (Made by @Xflicks)\n""")
